c72ad57000871f76844e5cbd1d795e62ca88f892
hexakon <hex4nova@gmail.com>
2026-07-22 15:29:55 +0200

    added navbar & IntersectionObserver to highlight where you're looking at

--- src/lib/MapList.svelte
+++ src/lib/MapList.svelte
@@ -3,11 +3,13 @@
     let {title, maplist} = $props();
 </script>
 
-<div class="my-8 text-white text-center">
-    <h1 class="mb-2 font-bold text-5xl">{title.toUpperCase()}</h1>
+<div class="relative my-8 text-white text-center">
+    <div id={title.replaceAll(" ","-")} class="-top-18 left-0 absolute"></div>
+    <h1 class="mb-2 font-bold text-2xl md:text-5xl">{title.toUpperCase()}</h1>
     <p><span class="text-lightpurple">{maplist.templates.length}</span> maps available in this category</p>
 </div>
-<div class="gap-3 grid grid-cols-3 mx-auto w-full max-w-300">
+<div class="relative gap-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto px-2 w-full max-w-300">
+    <div id={title.replaceAll(" ","-")+"_observer"} class="top-[50vh] right-0 bottom-0 left-0 -z-1 absolute"></div>
     {#each maplist.templates as map}
         <Map {...map} />
     {/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,50 @@
 <script lang="ts">
     import MapList from '$lib/MapList.svelte';
+	import { onMount } from 'svelte';
 
 	import minigame from '$lib/api/minigame.json'
 	import adventure from '$lib/api/adventure.json'
 	import experience from '$lib/api/experience.json'
 	import worldtemp from '$lib/api/worldtemp.json'
 	import inspiration from '$lib/api/inspiration.json'
+
+	const sections = ['minigames', 'adventures', 'experiences', 'world-templates', 'inspiration'];
+	let active = $state('minigames');
+
+	onMount(() => {
+		// set of all elements that are visible
+		const visible = new Set();
+
+		const observer = new IntersectionObserver(entries => {
+			for (const entry of entries) {
+				// minigames_observer -> minigames
+				const id = entry.target.id.slice(0, entry.target.id.indexOf('_'));
+				// mark visibility in the set
+				if (entry.isIntersecting) visible.add(id); else visible.delete(id);
+			}
+
+			const last = sections.filter(id => visible.has(id)).at(-1);
+			if (last) active = last;
+		})
+
+		for (const section of sections) {
+			const el = document.getElementById(section+'_observer');
+			if (el) observer.observe(el);
+		}
+
+		return () => observer.disconnect();
+	});
 </script>
 
+<nav class="top-0 left-0 z-2 fixed flex md:justify-center items-stretch bg-orange-900 w-full overflow-x-scroll text-white text-sm md:text-base text-center">
+	{#each sections as id}
+		<a class="flex items-center hover:bg-orange-600 px-4 py-2 transition-colors" class:bg-orange-600={active === id} href="#{id}">
+			{id.toUpperCase().replace('-', ' ')}
+		</a>
+	{/each}
+</nav>
+
+<div class="h-10"></div>
 
 <MapList title="minigames" maplist={minigame} />
 <MapList title="adventures" maplist={adventure} />
