1078b3eedd13ba54fe67821b4d845d2bd460e12e
hexakon <hex4nova@gmail.com>
2026-07-22 11:30:24 +0200

    fixed shadow colors, added api calls to realmshub

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+<h1 class="text-yellow">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<pre class="font-sans text-aqua">{JSON.stringify(data, null, 2)}</pre>
