ef5243b919c285ebd54faba1528e2a95ffc7b932
hexakon <hex4nova@gmail.com>
2026-07-22 13:21:13 +0200

    use github workflow to pull api once a day

--- src/lib/MapList.svelte
+++ src/lib/MapList.svelte
@@ -5,10 +5,10 @@
 
 <div class="my-8 text-white text-center">
     <h1 class="mb-2 font-bold text-5xl">{title.toUpperCase()}</h1>
-    <p><span class="text-lightpurple">{maplist.length}</span> maps live in this category</p>
+    <p><span class="text-lightpurple">{maplist.templates.length}</span> maps available in this category</p>
 </div>
 <div class="gap-3 grid grid-cols-3 mx-auto w-full max-w-300">
-    {#each maplist as map}
+    {#each maplist.templates as map}
         <Map {...map} />
     {/each}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
-    import Map from '$lib/Map.svelte';
     import MapList from '$lib/MapList.svelte';
-	import type { PageData } from './$types';
 
-	let { data }: { data: PageData } = $props();
+	import minigame from '$lib/api/minigame.json'
+	import adventure from '$lib/api/adventure.json'
+	import experience from '$lib/api/experience.json'
+	import worldtemp from '$lib/api/worldtemp.json'
+	import inspiration from '$lib/api/inspiration.json'
 </script>
 
 
-<MapList title="minigames" maplist={data.minigame} />
-<MapList title="adventures" maplist={data.adventure} />
-<MapList title="experiences" maplist={data.experience} />
-<MapList title="world templates" maplist={data.worldtemp} />
-<MapList title="inspiration" maplist={data.inspiration} />
+<MapList title="minigames" maplist={minigame} />
+<MapList title="adventures" maplist={adventure} />
+<MapList title="experiences" maplist={experience} />
+<MapList title="world templates" maplist={worldtemp} />
+<MapList title="inspiration" maplist={inspiration} />
